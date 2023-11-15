9feb7c0d8ed56b093d7f8ff18b621525bf4e4b5d
David Souza da Silva <142271970+Davidssz@users.noreply.github.com>
2023-11-14 21:35:06 -0300

    atividade7

---
+++ src/Mensalista.java
@@ -0,0 +1,33 @@
+
+public class Mensalista extends Empregado {
+
+    private String cargo;
+    // construtor
+
+    public Mensalista(String  n, String e, String c) {
+        super(n, e);
+        cargo = c;
+
+    }
+    // metodos de acesso
+
+    public void set_cargo(String c) {
+        cargo  = c;
+
+    }
+
+    public String get_cargo() {
+        return (cargo);
+
+    }
+
+    public void calcularSalario() {
+        if (cargo.equals("Junior")) {
+            salario = 2500;
+        } else if (cargo.equals("Pleno")) {
+            salario =  3500;
+        } else {
+            salario = 5500;
+        }
+    }
+}
